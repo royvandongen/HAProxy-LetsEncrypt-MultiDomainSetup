86005b5f6d65e4d56a6b47cab3a3bd7e4d83bc14
Roy van Dongen <royvandongen@users.noreply.github.com>
2020-03-15 22:18:24 +0100

    Create install-certs.sh

---
+++ install-certs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+  
+# Concatenate new cert files, with less output (avoiding the use tee and its output to stdout)
+for dir in /etc/letsencrypt/live/*; do
+    namedir="$(cut -d'/' -f5 <<< $dir)"
+    echo "Now fixing $namedir"
+        bash -c "cat /etc/letsencrypt/live/$namedir/fullchain.pem /etc/letsencrypt/live/$namedir/privkey.pem > /etc/haproxy/ssl/$namedir.pem"
+done
+
+# Reload  HAProxy
+service haproxy reload
