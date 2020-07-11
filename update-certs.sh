775122299bbeb266d87d76d76f848471807551ba
Roy van Dongen <royvandongen@users.noreply.github.com>
2020-07-11 21:02:59 +0200

    Update update-certs.sh
    Added check if path is directory, this caused issues with readme files.

--- update-certs.sh
+++ update-certs.sh
@@ -5,9 +5,11 @@ certbot renew --force-renewal --tls-sni-01-port=8888
 
 # Concatenate new cert files, with less output (avoiding the use tee and its output to stdout)
 for dir in /etc/letsencrypt/live/*; do
+  if [ -d "$dir" ]; then
     namedir="$(cut -d'/' -f5 <<< $dir)"
     echo "Now fixing $namedir"
         bash -c "cat /etc/letsencrypt/live/$namedir/fullchain.pem /etc/letsencrypt/live/$namedir/privkey.pem > /etc/haproxy/ssl/$namedir.pem"
+  fi
 done
 
 # Reload  HAProxy
