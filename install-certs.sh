67f2af545933e944953ab151acd3f8a3993e7eee
Roy van Dongen <royvandongen@users.noreply.github.com>
2020-07-11 21:02:14 +0200

    Update install-certs.sh
    Added check if path is directory, caused issues with readmy file.

--- install-certs.sh
+++ install-certs.sh
@@ -2,9 +2,11 @@
   
 # Concatenate new cert files, with less output (avoiding the use tee and its output to stdout)
 for dir in /etc/letsencrypt/live/*; do
+  if [ -d "$dir" ]; then
     namedir="$(cut -d'/' -f5 <<< $dir)"
     echo "Now fixing $namedir"
-        bash -c "cat /etc/letsencrypt/live/$namedir/fullchain.pem /etc/letsencrypt/live/$namedir/privkey.pem > /etc/haproxy/ssl/$namedir.pem"
+      bash -c "cat /etc/letsencrypt/live/$namedir/fullchain.pem /etc/letsencrypt/live/$namedir/privkey.pem > /etc/haproxy/ssl/$namedir.pem"
+  fi
 done
 
 # Reload  HAProxy
